b8d0c22664abbbe9c9e1a7a70257b6096fc0423a
patrick.klingler <patrick.klingler@clever-soft.de>
2022-11-11 10:06:11 +0100

    updat

--- src/main/java/CsvSplitterMain.java
+++ src/main/java/CsvSplitterMain.java
@@ -18,9 +18,11 @@ import java.util.*;
 public class CsvSplitterMain {
 
     private static Logger logger = LoggerFactory.getLogger(CsvSplitterMain.class);
-    private static String FILEPATH = "C:\\Users\\Patrick\\Desktop\\test\\";
-    private static String FILENAME = "DEKA_PRIIP_CLEVERSOFT_WERTE_20221026_20221026_201244.csv";
+    private static String SOURCE_FILEPATH = "C:\\Users\\patrick.klingler\\Downloads\\feri\\";
+    private static String SOURCE_FILENAME = "ferirat.csv";
     private static String DELIMITER = ";";
+    private static String TARGET_FILEPATH = "C:\\Users\\patrick.klingler\\Downloads\\feri\\";
+    private static String TARGET_FILENAME = "ferirat_splitted.csv";
 
 
     public static void main(String[] args) throws IOException {
@@ -28,7 +30,7 @@ public class CsvSplitterMain {
         Multimap<String, CSVRecord> map = ArrayListMultimap.create();
 
         logger.info("Reading CSV...");
-        Reader in = new FileReader(FILEPATH+FILENAME);
+        Reader in = new FileReader(SOURCE_FILEPATH+SOURCE_FILENAME);
         CSVFormat format = CSVFormat.newFormat(';').withRecordSeparator("\n");
         Iterable<CSVRecord> records = format.withFirstRecordAsHeader().withSkipHeaderRecord(false).parse(in);
         Set<String> headers = records.iterator().next().toMap().keySet();
@@ -41,10 +43,10 @@ public class CsvSplitterMain {
 
         int countISIN = map.keys().size();
         int currentPos = 0;
-        for(String isin : map.keys()) {
+        for(String isin : map.keySet()) {
             logger.info("Print {} of {} ISIN {}", currentPos, countISIN, isin);
             Collection<CSVRecord> rowsForIsin = map.get(isin);
-            try (CSVPrinter printer = new CSVPrinter(new FileWriter(FILEPATH+isin+"_splitted.csv"), format)) {
+            try (CSVPrinter printer = new CSVPrinter(new FileWriter(TARGET_FILEPATH+isin+"_"+TARGET_FILENAME), format)) {
                 printer.printRecord(Arrays.asList(headers.toArray()));
                 for(CSVRecord rec : rowsForIsin) {
                     printer.printRecord(rec);
